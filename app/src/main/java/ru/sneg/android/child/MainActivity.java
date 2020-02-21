f58c29bb08d394373fe6e0d3b27ac33620b8d267
Alexandrsneg <ai.kps42@gmail.com>
2020-02-21 17:23:00 +0700

    работающее приложение без косметики и ограничений по вводу

--- app/src/main/java/ru/sneg/android/child/MainActivity.java
+++ app/src/main/java/ru/sneg/android/child/MainActivity.java
@@ -14,13 +14,12 @@ public class MainActivity extends AppCompatActivity {
 
     private int countFamily = 0;
     private int countChild = 0;
-
+    private float flatCoast = 0;
+    private float matCap = 0;
 
 
     public void btnFamily(View view) {
-
         displayFam(++countFamily);
-
         if (countFamily == 6) {
             displayFam(0);
             countFamily = 0;
@@ -28,15 +27,12 @@ public class MainActivity extends AppCompatActivity {
 
     }
     public void btnChild(View view) {
-
         displayChild(++countChild);
-
         if (countChild == 5) {
             displayChild(0);
             countChild = 0;
         }
     }
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -61,11 +57,18 @@ public class MainActivity extends AppCompatActivity {
                     Toast.LENGTH_LONG).show();
             return;
         }
+        // считывание сумм квартиры и сертификата с EditText(ов)
+        flatCoast = Float.parseFloat(inputEditText1.getText().toString());
+        matCap = Float.parseFloat(inputEditText2.getText().toString());
+
+        float childPart = (matCap/flatCoast/countFamily);
+        displayChildPart(childPart);
+
+        float sharedParentsPart = (1 - childPart*countChild);
+        displaySharedParentsPart(sharedParentsPart);
 
     }
-
-
-
+    
     private void displayFam(int countFamily) {
         TextView quantityTextView = findViewById(R.id.textView2);
         quantityTextView.setText("x" + countFamily);
@@ -74,6 +77,16 @@ public class MainActivity extends AppCompatActivity {
     private void displayChild(int countChild) {
         TextView quantityTextView =  findViewById(R.id.textView);
         quantityTextView.setText("x" + countChild);
+    }
+
+    private void displayChildPart(float childPart) {
+        TextView quantityTextView =  findViewById(R.id.textView4);
+        quantityTextView.setText("" + childPart);
+    }
+
+    private void displaySharedParentsPart(float sharedParentsPart) {
+        TextView quantityTextView =  findViewById(R.id.textView6);
+        quantityTextView.setText("" + sharedParentsPart);
     }
 
 
